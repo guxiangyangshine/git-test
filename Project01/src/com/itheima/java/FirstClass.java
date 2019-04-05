de00bdd1775090dca9296f12d43251b1f81af1d9
guxiangyang <648937190@qq.com>
2019-04-05 12:39:52 +0800

    把本地工程添加到git

---
+++ Project01/src/com/itheima/java/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.java;
+
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
